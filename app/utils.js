875e0d143e582e389e5293133971258dcdc15a63
Juho Hautala <juho.hautala@helsinki.fi>
2015-12-07 00:14:11 +0200

    url util func

--- app/utils.js
+++ app/utils.js
@@ -1,8 +1,16 @@
 import { curry } from 'ramda';
 
+const url = (term) => {
+  return 'https://api.flickr.com/services/feeds/photos_public.gne?tags=' +
+    term + '&format=json&jsoncallback=?';
+};
+
 const trace = curry((s, x) => {
-  console.log(s);
+  console.log(s, x);
   return x;
 });
 
-export { trace };
+export {
+  url,
+  trace,
+};
