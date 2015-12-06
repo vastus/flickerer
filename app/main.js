d0ad8d71fd48b815b0753a9cd0a7b1a322d736ed
Juho Hautala <juho.hautala@helsinki.fi>
2015-12-07 00:15:29 +0200

    display images given to app

--- app/main.js
+++ app/main.js
@@ -1,3 +1,48 @@
-import { trace } from './utils';
+import $ from 'jquery';
+import {
+  compose,
+  curry,
+  map,
+  prop,
+} from 'ramda';
 
-console.log(trace('ready...', 'check: app/main.js'));
+import {
+  url,
+  trace,
+} from './utils';
+
+const Impure = function ({ $, curry }) {
+  const getJSON = curry((callback, url) => {
+    $.getJSON(url, callback);
+  });
+
+  const setHTML = curry((selector, html) => {
+    $(selector).html(html);
+  });
+
+  return {
+    getJSON,
+    setHTML,
+  };
+}({ $, curry });
+
+
+// mediaURL :: Object -> Prop
+const mediaURL = compose(prop('m'), prop('media'));
+
+// srcs :: Object -> [mediaURL]
+const srcs = compose(map(mediaURL), prop('items'));
+
+// img :: URL -> <img src=url />
+const img = (url) => $('<img />', { src: url });
+
+// images
+const images = compose(map(img), srcs);
+
+// renderImages
+const renderImages = compose(Impure.setHTML('body'), images);
+
+// app
+const app = compose(Impure.getJSON(renderImages), url);
+
+app('cats');
